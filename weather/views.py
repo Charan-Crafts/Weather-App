1cbd6f86d6bb62eb570cb58d2aae9b35ec4bee0c
Charan <mangalacharancharan@gmail.com>
2024-11-14 14:37:40 +0530

    Initial Commit

--- weather/views.py
+++ weather/views.py
@@ -5,7 +5,6 @@ from .forms import CityForm
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
-
     err_msg = ''
     message = ''
     message_class = ''
